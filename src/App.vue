d71aa208678065b61e97e32a7a20e8761032aa6a
Jeshan Chhabra <jeshanchhabra@yahoo.com>
2025-05-14 12:06:29 -0400

    Added job listing component card and added json file as backend data

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,11 @@
   import Navbar from './components/Navbar.vue';
   import Hero from './components/Hero.vue';
   import HomeCards from './components/HomeCards.vue';
+  import JobLIstings from './components/JobLIstings.vue';
 </script>
 <template>
   <Navbar/>
   <Hero title="Test title" subtitle="Test Subtitle"/>
   <HomeCards/>
+  <JobLIstings :limit="3" :show-btn="true"/>
 </template>
